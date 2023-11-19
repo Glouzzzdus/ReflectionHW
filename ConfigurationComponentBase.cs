d06107d83fe763916daee957ad7c656049188dc3
Andrii Hluzdovskyi <Andrii_Hluzdovskyi@epam.com>
2023-11-19 20:43:35 +0100

    Mess but need to commit

--- ConfigurationComponentBase.cs
+++ ConfigurationComponentBase.cs
@@ -1,197 +1,59 @@
-﻿using System;
+﻿using HWReflection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using HWReflection;
-using System.Xml.Linq;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using Newtonsoft.Json.Linq;
-using System.Runtime.CompilerServices;
-using System.Linq;
-using System.Collections.Generic;
 
-public abstract class ConfigurationComponentBase
+public class ConfigurationComponentBase
 {
-    private Dictionary<ProviderType, Action> loadActions;
-    private Dictionary<ProviderType, Action> saveActions;
+    private IDictionary<ProviderType, IConfigurationProvider> _configurationProviders;
 
     public ConfigurationComponentBase()
     {
-        loadActions = new Dictionary<ProviderType, Action>
-        {
-            { ProviderType.File, LoadXmlSettings },
-            { ProviderType.ConfigurationManager, LoadJsonSettings }
-        };
-
-        saveActions = new Dictionary<ProviderType, Action>
-        {
-            { ProviderType.File, SaveXmlSettings },
-            { ProviderType.ConfigurationManager, SaveJsonSettings }
-        };
+        _configurationProviders = new Dictionary<ProviderType, IConfigurationProvider>();
+        LoadPlugins();
     }
 
-    public void SaveSettings()
+    private void LoadPlugins()
     {
-        var providerTypes = GetDistinctProviderTypes();
-        foreach (var providerType in providerTypes)
+        string pluginsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "plugins");
+        if (!Directory.Exists(pluginsPath))
         {
-            saveActions[providerType].Invoke();
-        }
-    }
-
-    public void LoadSettings()
-    {
-        var providerTypes = GetDistinctProviderTypes();
-        foreach (var providerType in providerTypes)
-        {
-            loadActions[providerType].Invoke();
-        }
-    }
-
-    private HashSet<ProviderType> GetDistinctProviderTypes()
-    {
-        return new HashSet<ProviderType>(this.GetType().GetProperties()
-            .Select(p => Attribute.GetCustomAttribute(p, typeof(ConfigurationItemAttribute)) as ConfigurationItemAttribute)
-            .Where(attr => attr != null)
-            .Select(attr => attr.ProviderType)
-            .Distinct());
-    }
-
-    private void SaveJsonSettings()
-    {
-        string outputFolderPath = AppDomain.CurrentDomain.BaseDirectory;
-        var configurationBuilder = new ConfigurationBuilder()
-            .SetBasePath(outputFolderPath)
-            .AddJsonFile("appsettings.json");
-
-        var configuration = configurationBuilder.Build();
-        var configRoot = configuration as ConfigurationRoot;
-        var jsonFile = configRoot.Providers
-            .FirstOrDefault(p => p.GetType() == typeof(JsonConfigurationProvider)) as JsonConfigurationProvider;
-
-        if (jsonFile == null)
-        {
-            throw new InvalidOperationException("appsettings.json was not found.");
+            throw new DirectoryNotFoundException($"Directory '{pluginsPath}' not found.");
         }
 
-        JObject fileContent;
-
-        using (var streamReader = new StreamReader(jsonFile.Source.Path))
+        foreach (var file in Directory.GetFiles(pluginsPath, "*.dll"))
         {
-            fileContent = JObject.Parse(streamReader.ReadToEnd());
-        }
-
-        foreach (PropertyInfo property in GetType().GetProperties())
-        {
-            var attr = (ConfigurationItemAttribute)Attribute.GetCustomAttribute(property, typeof(ConfigurationItemAttribute));
-            if (attr != null && attr.ProviderType == ProviderType.ConfigurationManager)
+            Assembly assembly = Assembly.LoadFrom(file);
+            foreach (Type type in assembly.GetTypes())
             {
-                JToken settings = fileContent["ApplicationSettings"];
-                settings[attr.SettingName] = JToken.FromObject(property.GetValue(this));
-            }
-        }
-
-        using (var streamWriter = new StreamWriter(jsonFile.Source.Path))
-        {
-            streamWriter.Write(fileContent.ToString());
-        }
-    }
-
-    private void SaveXmlSettings()
-    {
-        string outputFolderPath = AppDomain.CurrentDomain.BaseDirectory;
-        string xmlFilePath = Path.Combine(outputFolderPath, "appsettings.xml");
-        XDocument xmlDoc = XDocument.Load(xmlFilePath);
-        XElement xmlAppSettings = xmlDoc.Element("Settings").Element("ApplicationSettings");
-
-        foreach (PropertyInfo property in GetType().GetProperties())
-        {
-            var attr = (ConfigurationItemAttribute)Attribute.GetCustomAttribute(property, typeof(ConfigurationItemAttribute));
-            if (attr != null && attr.ProviderType == ProviderType.File)
-            {
-                XElement settingElement = xmlAppSettings.Element(attr.SettingName);
-
-                if (property.PropertyType == typeof(TimeSpan))
+                if (typeof(IConfigurationProvider).IsAssignableFrom(type) && !type.IsInterface)
                 {
-                    settingElement.Value = ((TimeSpan)property.GetValue(this)).ToString();
-                }
-                else
-                {
-                    settingElement.Value = property.GetValue(this).ToString();
-                }
-            }
-        }
-
-        xmlDoc.Save(xmlFilePath);
-    }
-
-    private void LoadJsonSettings()
-    {
-        string outputFolderPath = AppDomain.CurrentDomain.BaseDirectory;
-        var configurationBuilder = new ConfigurationBuilder()
-            .SetBasePath(outputFolderPath)
-            .AddJsonFile("appsettings.json");
-
-        var configuration = configurationBuilder.Build();
-
-        foreach (PropertyInfo property in GetType().GetProperties())
-        {
-            var attr = (ConfigurationItemAttribute)Attribute.GetCustomAttribute(property, typeof(ConfigurationItemAttribute));
-            if (attr != null && attr.ProviderType == ProviderType.ConfigurationManager)
-            {
-                string configRawValue = configuration[$"ApplicationSettings:{attr.SettingName}"];
-
-                if (property.PropertyType == typeof(TimeSpan))
-                {
-                    if (TimeSpan.TryParse(configRawValue, out TimeSpan timeSpanValue))
-                    {
-                        property.SetValue(this, timeSpanValue);
-                    }
-                    else
-                    {
-                        throw new ArgumentException($"Can't convert '{configRawValue}' property {property.Name} to TimeSpan type.");
-                    }
-                }
-                else
-                {
-                    object value = Convert.ChangeType(configRawValue, property.PropertyType);
-                    property.SetValue(this, value);
+                    IConfigurationProvider providerInstance = (IConfigurationProvider)Activator.CreateInstance(type);
+                    _configurationProviders.Add(providerInstance.ProviderType, providerInstance);
                 }
             }
         }
     }
 
-    private void LoadXmlSettings()
+    public void LoadSettings(ProviderType providerType, MyConfigurationClass configClass)
     {
-        string outputFolderPath = AppDomain.CurrentDomain.BaseDirectory;
-        string xmlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.xml");
-        XDocument xmlDoc = XDocument.Load(xmlFilePath);
-        XElement xmlAppSettings = xmlDoc.Element("Settings").Element("ApplicationSettings");
-
-        foreach (PropertyInfo property in GetType().GetProperties())
+        if (!_configurationProviders.ContainsKey(providerType))
         {
-            var attr = (ConfigurationItemAttribute)Attribute.GetCustomAttribute(property, typeof(ConfigurationItemAttribute));
-            if (attr != null && attr.ProviderType == ProviderType.File)
-            {
-                string rawValue = xmlAppSettings.Element(attr.SettingName).Value;
-
-                if (property.PropertyType == typeof(TimeSpan))
-                {
-                    if (TimeSpan.TryParse(rawValue, out TimeSpan timeSpanValue))
-                    {
-                        property.SetValue(this, timeSpanValue);
-                    }
-                    else
-                    {
-                        throw new ArgumentException($"Can't convert '{rawValue}' property {property.Name} to TimeSpan type.");
-                    }
-                }
-                else
-                {
-                    object value = Convert.ChangeType(rawValue, property.PropertyType);
-                    property.SetValue(this, value);
-                }
-            }
+            throw new InvalidOperationException($"No provider found for ProviderType '{providerType}'.");
         }
+
+        _configurationProviders[providerType].LoadSettingsFromSource(configClass);
+    }
+
+    public void SaveSettings(ProviderType providerType, MyConfigurationClass configClass)
+    {
+        if (!_configurationProviders.ContainsKey(providerType))
+        {
+            throw new InvalidOperationException($"No provider found for ProviderType '{providerType}'.");
+        }
+
+        _configurationProviders[providerType].SaveSettingsToSource(configClass);
     }
 }
---
+++ IConfigurationProvider.cs
@@ -0,0 +1,16 @@
+﻿using HWReflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionHW
+{
+    public interface IConfigurationProvider
+    {
+        ProviderType ProviderType { get; }
+        void LoadSettingsFromSource(MyConfigurationClass configClass);
+        void SaveSettingsToSource(MyConfigurationClass configClass);
+    }
+}
