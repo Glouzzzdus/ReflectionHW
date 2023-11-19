a3cdcfefdffec2be19a559633c8862a209efb657
Andrii Hluzdovskyi <Andrii_Hluzdovskyi@epam.com>
2023-11-19 21:09:21 +0100

    Another way

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using HWReflection;
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace ConsoleApp
@@ -8,8 +9,10 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             MyConfigurationClass myConfig = new MyConfigurationClass();
+            ConfigurationComponentBase configComponent = new ConfigurationComponentBase();
 
-            myConfig.LoadSettings();
+
+            configComponent.LoadSettings(ProviderType.File, myConfig);
 
             Console.WriteLine("=== Before changes ===");
             Console.WriteLine("Setting1: " + myConfig.Setting1);
@@ -17,6 +20,9 @@ namespace ConsoleApp
             Console.WriteLine("Setting3: " + myConfig.Setting3);
             Console.WriteLine("Setting4: " + myConfig.Setting4);
             Console.WriteLine("Setting5: " + myConfig.Setting5);
+
+            configComponent.LoadSettings(ProviderType.ConfigurationManager, myConfig);
+
             Console.WriteLine("Setting6: " + myConfig.Setting6);
             Console.WriteLine("Setting7: " + myConfig.Setting7);
             Console.WriteLine("Setting8: " + myConfig.Setting8);
@@ -28,15 +34,18 @@ namespace ConsoleApp
             myConfig.Setting3 = 77.77;
             myConfig.Setting4 = TimeSpan.FromSeconds(10);
             myConfig.Setting5 = new DateTime(2025, 12, 31);
+
+            configComponent.SaveSettings(ProviderType.File, myConfig);
+
             myConfig.Setting6 = "New Message 1";
             myConfig.Setting7 = 789;
             myConfig.Setting8 = 88.88;
             myConfig.Setting9 = TimeSpan.FromSeconds(40);
             myConfig.Setting10 = new DateTime(1999, 01, 01);
 
-            myConfig.SaveSettings();
+            configComponent.SaveSettings(ProviderType.File, myConfig);
 
-            myConfig.LoadSettings();
+            configComponent.LoadSettings(ProviderType.ConfigurationManager, myConfig);
 
             Console.WriteLine("=== After changes ===");
             Console.WriteLine("Setting1: " + myConfig.Setting1);
@@ -44,6 +53,9 @@ namespace ConsoleApp
             Console.WriteLine("Setting3: " + myConfig.Setting3);
             Console.WriteLine("Setting4: " + myConfig.Setting4);
             Console.WriteLine("Setting5: " + myConfig.Setting5);
+
+            configComponent.LoadSettings(ProviderType.ConfigurationManager, myConfig);
+
             Console.WriteLine("Setting6: " + myConfig.Setting6);
             Console.WriteLine("Setting7: " + myConfig.Setting7);
             Console.WriteLine("Setting8: " + myConfig.Setting8);
